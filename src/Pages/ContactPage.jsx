d347fe7a6cdfacc8eb5803482356d6fe0ae69f91
wahyu2021 <wahyuwn256@gmail.com>
2025-05-26 10:39:02 +0700

    Day-1

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./Navbar";
+import Navbar from "./NavBar";
 import Footer from "./Footer";
 import { db } from "../Firebase";
 import { collection, addDoc } from "firebase/firestore";
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "./NavBar"
 import { FaArrowRight } from "react-icons/fa6";
 import { FiFilter } from "react-icons/fi";
 import Footer from "./Footer";
